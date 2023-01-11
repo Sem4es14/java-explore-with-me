1763bc8d68941c2e4c1bfb2ba1a6e624374b8531
Semyon <ily-senin@yandex.ru>
2023-01-11 19:31:12 +0300

    delete unused

--- ewm-main-service/src/main/java/ru/yandex/ewmmain/compilations/mapper/CompilationsMapper.java
+++ ewm-main-service/src/main/java/ru/yandex/ewmmain/compilations/mapper/CompilationsMapper.java
@@ -1,14 +1,10 @@
 package ru.yandex.ewmmain.compilations.mapper;
 
-import ru.yandex.ewmmain.client.ewmclient.EwmClient;
 import ru.yandex.ewmmain.compilations.model.Compilation;
 import ru.yandex.ewmmain.compilations.responsedto.CompilationDto;
-import ru.yandex.ewmmain.event.mapper.EventMapper;
 import ru.yandex.ewmmain.event.responsedto.EventShortDto;
-import ru.yandex.ewmmain.participationrequest.repository.RequestRepository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CompilationsMapper {
     public static CompilationDto fromCompilationToDto(Compilation compilation,
