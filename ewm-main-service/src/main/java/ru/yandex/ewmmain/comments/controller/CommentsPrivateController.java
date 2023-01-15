86aade814d37cbce09470018819ad4dffeda6fea
Semyon <ily-senin@yandex.ru>
2023-01-15 18:12:56 +0300

    delete unused

--- ewm-main-service/src/main/java/ru/yandex/ewmmain/comments/controller/CommentsPrivateController.java
+++ ewm-main-service/src/main/java/ru/yandex/ewmmain/comments/controller/CommentsPrivateController.java
@@ -1,7 +1,6 @@
 package ru.yandex.ewmmain.comments.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.ewmmain.comments.requestdto.CommentCreateRequest;
